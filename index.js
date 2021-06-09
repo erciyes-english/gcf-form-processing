088b310bb6a9f290fb6d68c049339e0018e0517c
Clay Harmon <clay@clayharmon.me>
2021-06-09 16:31:10 +0300

    Add Google sheet api stuff

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ require("dotenv").config();
 
 const Joi = require("joi");
 const { google } = require("googleapis");
-const { promisify } = require("util");
 
 const leadFormSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   email: Joi.string().email().required(),
 });
+
+const addGoogleSheet = async (values) => {
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  const api = google.sheets("v4");
+  const addValues = api.spreadsheets.values.append({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: "Sheet1",
+    valueInputOption: "RAW",
+    insertDataOption: "INSERT_ROWS",
+    resource: {
+      values: [values],
+    },
+    auth,
+  });
+  return addValues;
+};
 
 exports.main = async (req, res) => {
   if (req.method !== "POST") {
@@ -18,7 +36,8 @@ exports.main = async (req, res) => {
   const { body } = req;
   try {
     const value = await leadFormSchema.validateAsync(body);
-    return res.status(200).send("Valid");
+    const stuff = JSON.stringify(await addGoogleSheet(Object.values(body)));
+    return res.status(200).send(stuff);
   } catch (err) {
     return res.status(400).send(err);
   }
